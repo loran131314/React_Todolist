2cefbf2ad2541c31ea90cb27947a7d5844315edc
Laron <laron9486wk@gmail.com>
2023-09-06 18:01:53 +0800

    Version_2.1_remove verification message

--- src/componenets/LogIn.jsx
+++ src/componenets/LogIn.jsx
@@ -26,29 +26,33 @@ function LogIn() {
   const logIn = () => {
     (async () => {
       // console.log('logIn form:', userData);    // TEST
-      if (!userData.email || !userData.password) {
-        Swal.fire('Failed!', 'Please rewrite the sign up information!');
-        return;
-      }
-      setIsLoading(true)
-      try {
-        const res = await axios.post(`${VITE_APP_HOST}/users/sign_in`, userData)
-        // console.log(res);    // TEST
-        const { data } = await res;
-        // console.log(token);    // TEST
+      // if (!userData.email || !userData.password) {
+      //   Swal.fire('Failed!', 'Please rewrite the sign up information!');
+      //   return;
+      // }
+      if (userData.email && userData.password) {
+        setIsLoading(true)
+        try {
+          const res = await axios.post(`${VITE_APP_HOST}/users/sign_in`, userData)
+          // console.log(res);    // TEST
+          const { data } = await res;
+          // console.log(token);    // TEST
 
-        document.cookie = `token=${data.token}; SameSite=None; Secure`    // 把用戶的 token 存入 web cookie
+          document.cookie = `token=${data.token}; SameSite=None; Secure`    // 把用戶的 token 存入 web cookie
 
-        Swal.fire({
-          title: 'Success!',
-          text: 'You logged in!  Please wait!',
-          showConfirmButton: false,
-          timer: 1500
-        }).then(() => { setIsLoading(false); nevigate('/todoList'); })
-      } catch (err) {
-        console.log(err);
-        Swal.fire('Failed!', err.response.data.message);
-        setIsLoading(false);
+          Swal.fire({
+            title: 'Success!',
+            text: 'You logged in!  Please wait!',
+            showConfirmButton: false,
+            timer: 1500
+          }).then(() => { setIsLoading(false); nevigate('/todoList'); })
+        } catch (err) {
+          console.log(err);
+          Swal.fire('Failed!', err.response.data.message);
+          setIsLoading(false);
+        }
+      }else{
+        
       }
     })()
   }
@@ -62,6 +66,7 @@ function LogIn() {
         <span>此欄位不可留空</span>
         <label className="formControls_label" htmlFor="password">密碼</label>
         <input className="formControls_input" type="password" name="password" id="password" placeholder="請輸入密碼" onChange={handelInput} required />
+        <span>此欄位不可留空</span>
         <input className="formControls_btnSubmit" type="button" onClick={logIn} disabled={isLoading /* 避免用戶重複點擊 */} value="登入" />
         <NavLink className="formControls_btnLink" to="/signUp">註冊帳號</NavLink>
       </form>
