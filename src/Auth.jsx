c649b0c3bee28cd648c1128d8092e9619b774976
K <100009235+loran131314@users.noreply.github.com>
2024-05-21 18:12:00 +0800

    Update Auth.jsx

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -7,8 +7,8 @@ function Auth() {
       <div className="bg-yellow">
         <div className="conatiner loginPage signUpPage vhContainer">
           <div className="side">
-            <img className="logoImg" src="https://upload.cc/i1/2022/03/23/rhefZ3.png" alt="Logo" />
-            <img className="d-m-n" src="https://upload.cc/i1/2022/03/23/tj3Bdk.png" alt="Work Image" />
+            <img className="logoImg" src="/Logo.png" alt="Logo" />
+            <img className="d-m-n" src="/Work.png" alt="Work Image" />
           </div>
           <div>
             <Outlet/>
