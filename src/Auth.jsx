c212d37d42333477b5b389eba23253ba47dcb35a
Laron <laron9486wk@gmail.com>
2024-05-21 20:11:17 +0800

    img path change

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -7,8 +7,8 @@ function Auth() {
       <div className="bg-yellow">
         <div className="conatiner loginPage signUpPage vhContainer">
           <div className="side">
-            <img className="logoImg" src="/Logo.png" alt="Logo" />
-            <img className="d-m-n" src="/Work.png" alt="Work Image" />
+            <img className="logoImg" src="/logo.png" alt="Logo" />
+            <img className="d-m-n" src="/work.png" alt="Work Image" />
           </div>
           <div>
             <Outlet/>
