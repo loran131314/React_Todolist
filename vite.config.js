997d85319fefb97611d7dee29b21403a392977a0
Laron <laron9486wk@gmail.com>
2024-05-21 20:28:23 +0800

    base path change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: process.env.NODE_ENV === 'production' ? '/React_Todolist/' : '/',
-  base: "/React_Todolist/",
+  base: "./",
   plugins: [react()],
 })
